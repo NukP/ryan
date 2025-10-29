685467c7bcf60f19150030f8d01598635ba5264b
NukP <nukorn.p27@gmail.com>
2025-10-29 13:26:41 +0100

    Finish description for analyzer package.

--- src/ryan/analyzer/__init__.py
+++ src/ryan/analyzer/__init__.py
@@ -2,4 +2,11 @@
 analyzer
 ========
 This package contains modules for analyzing and plotting raw electrochemical CO2 / CO experiment data.
+
+This modules is designed to be working with a folder structure shown in examples/Data analysis. Simply run Run.ipynb file. 
+submodules:
+- autoanalyze: Prepare raw experimentn data and parse it to underlying yadg/dgpost script for analysis.
+- autoplot: Plot processed experiment data using predefined plotting functions.
+- autopybis: Send raw and processed data to OpenBis via Pybis.
+- auxiliary: Helper functions for data processing and analysis.
 """
